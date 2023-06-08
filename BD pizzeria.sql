ef8f44fb1154c576a499b75338fb6b31aba3ef20
Somarriba hi <128437648+somarriba02polz@users.noreply.github.com>
2023-06-08 17:30:37 -0600

    Update BD pizzeria.sql

--- BD pizzeria.sql
+++ BD pizzeria.sql
@@ -2,11 +2,13 @@
 creacion de una base de datos para una pizzeria*/
 
 create table Clientes(
- Id_cliente int identity(1,1) primary key,
+cedula_C nvarchar(15) check (cedula_C LIKE '[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][A-Z]'),
+Id_cliente int identity(1,1) primary key,
 Tel_C nvarchar(8) check (Tel_C LIKE '[78][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
 Dir_C nvarchar(100),
 PNC nvarchar(30) not null,
 SNC nvarchar(30),
 PAC nvarchar(30) not null,
 SAC nvarchar(30),
+ 
  /*que mas podemos agregar?*/
