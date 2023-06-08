30930a8f36121b9f9e8c46c8c8172bff1ad420ab
Somarriba hi <128437648+somarriba02polz@users.noreply.github.com>
2023-06-08 17:50:55 -0600

    Update BD pizzeria.sql

--- BD pizzeria.sql
+++ BD pizzeria.sql
@@ -3,15 +3,22 @@ creacion de una base de datos para una pizzeria*/
 
 create table Clientes(
 cedula_C nvarchar(15) check (cedula_C LIKE '[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][A-Z]'),
-Id_cliente int identity(1,1) primary key,
+Id_Cliente int identity(1,1) primary key,
 Tel_C nvarchar(8) check (Tel_C LIKE '[78][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
 Dir_C nvarchar(100),
 PNC nvarchar(30) not null,
 SNC nvarchar(30),
 PAC nvarchar(30) not null,
 SAC nvarchar(30),
-foreign key (Num_pedido) references Pedidos(Num_pedido),
-foreign key (Id_pedido) references Pedidos(Id_pedido)
+foreign key (Num_pedido) references Pedido(Num_pedido),
+foreign key (Id_pedido) references Pedido(Id_pedido),
 ),
+
+create table Pedido(
+ Id_pedido int identity(1,1) primary key,
+ Fecha_pedido datetime default getdate() not null,
+ foreign key (PNC) references Clientes(PNC),
+ foreign key (Id_Cliente) references Clientes(Id_Cliente),
+ 
  
  /*que mas podemos agregar?*/
