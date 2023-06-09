4f431c24d59db1812cf9dc500bac458580fbe769
Somarriba hi <128437648+somarriba02polz@users.noreply.github.com>
2023-06-08 18:54:06 -0600

    Update BD pizzeria.sql

--- BD pizzeria.sql
+++ BD pizzeria.sql
@@ -10,30 +10,45 @@ PNC nvarchar(30) not null,
 SNC nvarchar(30),
 PAC nvarchar(30) not null,
 SAC nvarchar(30),
-foreign key (Num_pedido) references Pedido(Num_pedido),
-foreign key (Id_pedido) references Pedido(Id_pedido),
 ),
 
 create table Pedido(
- Id_pedido int identity(1,1) primary key,
- Num_pedido int identity(1,1),
- Fecha_pedido datetime default getdate() not null,
+ Id_Pedido int identity(1,1) primary key,
+ Num_Pedido int identity(1,1),
+ Fecha_Pedido datetime default getdate() not null,
  foreign key (PNC) references Clientes(PNC),
  foreign key (Id_Cliente) references Clientes(Id_Cliente),
  Metodo_pago nvarchar(20),
  Total_pago money,
  Id_pizza int,
- foreign key (Id_pizza) references Productos(Id_pizza)
+ foreign key (Id_pizza) references Pizzas(Id_pizza)
  ),
  
  create table Pizzas(
-  Id_pizza int identity(1,1) primary key,
-  precio_pizza money not null,
+  Id_Pizza int identity(1,1) primary key,
+  precio_Pizza money not null,
   slice int not null,
+  Ingredientes_Pizza nvarchar(60) not null,
   Descripcion nvarchar(50) not null,
   )
   
-  create table
+  create table Entrega(
+   foreign key(Id_pedido) references Pedido(Id_pedido),
+   FechaHoraEntrega DATETIME DEFAULT GETDATE(),
+   Dir_entrega nvarchar(50) not null,
+   Id_Empleado int,
+   foreign key(Id_Empleado) references Empleados(Id_Empleado),
+   foreign key(Id_Pedido) references Pedido(Id_Pedido),
+   Tipo_Entrega nvarchar(20)
+   )
+   
+   create table Pago(
+    Id_pago int identity(1,1) primary key,
+    foreign key(Id_Pedido) references Pedido(Id_Pedido),
+    Metodo_pago
+    
+   
+   
  
  
  /*que mas podemos agregar?*/
