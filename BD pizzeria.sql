795eb234a3e3a5bad482071a663e735ace1ff012
Denzel Tiffer <94322765+Bframe-max@users.noreply.github.com>
2023-06-08 23:56:48 -0600

    Update BD pizzeria.sql
    actualizacion , codigo ejectando correctamente

--- BD pizzeria.sql
+++ BD pizzeria.sql
@@ -1,65 +1,43 @@
 /* Proyecto para la clase de base de datos
 creacion de una base de datos para una pizzeria*/
 
-create database Pizzeria
-use Pizzeria
+CREATE DATABASE Pizzeria
+USE Pizzeria
 
-create table Clientes(
-Id_Cliente int identity(1,1) primary key not null,
-PNC nvarchar(30) not null,
-SNC nvarchar(30),
-PAC nvarchar(30) not null,
-SAC nvarchar(30),
-cedula_C nvarchar(15) check (cedula_C LIKE '[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][A-Z]'),
-Tel_C nvarchar(8) check (Tel_C LIKE '[78][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
-Dir_C nvarchar(100),
-Email nvarchar(100),
+CREATE TABLE Clientes (
+  Id_Cliente INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  PNC NVARCHAR(30) NOT NULL,
+  SNC NVARCHAR(30),
+  PAC NVARCHAR(30) NOT NULL,
+  SAC NVARCHAR(30),
+  cedula_C NVARCHAR(15) CHECK (cedula_C LIKE '[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][A-Z]'),
+  Tel_C NVARCHAR(8) CHECK (Tel_C LIKE '[78][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+  Dir_C NVARCHAR(100),
+  Email NVARCHAR(100)
 )
 
-create table Pizzas(
-  Id_Pizza int identity(1,1) primary key not null,
-  precio_Pizza money not null,
-  slice int not null,
-  Ingredientes_Pizza nvarchar(60) not null,
-  Descripcion nvarchar(50) not null,
-  )
+CREATE TABLE Empleado (
+  Id_Empleado INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  PN_Empleado NVARCHAR(30) NOT NULL,
+  SN_Empleado NVARCHAR(30),
+  PA_Empleado NVARCHAR(30) NOT NULL,
+  SA_Empleado NVARCHAR(30),
+  cedula_Empleado NVARCHAR(15) CHECK (cedula_Empleado LIKE '[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][A-Z]'),
+  Tel_E NVARCHAR(8) CHECK (Tel_E LIKE '[78][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+  Email NVARCHAR(100),
+  FechaContratacion DATE,
+  Salario DECIMAL(10, 2),
+  HorasTrabajo INT,
+  FechaNacimiento DATE,
+  EstadoCivil VARCHAR(20),
+  Genero VARCHAR(10),
+  NumeroCuentaBancaria VARCHAR(20)
+)
 
-create table Pedido(
- Id_Pedido int identity(1,1) primary key not null,
- Num_Pedido int ,
- Fecha_Pedido datetime default getdate() not null,
- PNC nvarchar(30),
- Id_Cliente int,
- foreign key (Id_Cliente) references Clientes(Id_Cliente),
- Total_pago money,
- Id_pizza int,
- foreign key (Id_pizza) references Pizzas(Id_pizza)
- )
- 
- 
-  
-  create table Empleado(
-   Id_Empleado  int identity(1,1) primary key not null,
-   PN_Empleado nvarchar(30) not null,
-   SN_Empleado nvarchar(30),
-   PA_Empleado nvarchar(30) not null,
-   SA_Empleado nvarchar(30),
-   cedula_Empleado nvarchar(15) check (cedula_Empleado LIKE '[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][A-Z]'),
-   Tel_E nvarchar(8) check (Tel_E LIKE '[78][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
-   Email nvarchar(100),
-   FechaContratacion DATE,
-   Salario DECIMAL(10, 2),
-   HorasTrabajo INT,
-   FechaNacimiento DATE,
-   EstadoCivil VARCHAR(20),
-   Genero VARCHAR(10),
-   NumeroCuentaBancaria VARCHAR(20),
-  )
-   
-  CREATE TABLE PuestoEmpleado (
-  Id_Puesto INT PRIMARY KEY not null,
-  Puesto nvarchar(50)
-  )
+CREATE TABLE PuestoEmpleado (
+  Id_Puesto INT PRIMARY KEY NOT NULL,
+  Puesto NVARCHAR(50)
+)
 
 CREATE TABLE Empleado_Puesto (
   Id_Empleado INT,
@@ -72,7 +50,7 @@ INSERT INTO PuestoEmpleado (Id_Puesto, Puesto) VALUES
 (1, 'Cocinero'),
 (2, 'Repartidor'),
 (3, 'Supervisor'),
-(4, 'Mercadeo');
+(4, 'Mercadeo')
 
 INSERT INTO Empleado_Puesto (Id_Empleado, Id_Puesto) VALUES
 (DEFAULT, 1), -- Juan Perez es cocinero
@@ -80,45 +58,88 @@ INSERT INTO Empleado_Puesto (Id_Empleado, Id_Puesto) VALUES
 (DEFAULT, 3), -- Pedro Lopez es supervisor
 (DEFAULT, 4) -- Laura Torres está en mercadeo
 
-   
-   
-  create table Entrega(
-   ID_Pedido int,
-   foreign key(Id_Pedido) references Pedido(Id_Pedido),
-   FechaHoraEntrega DATETIME DEFAULT GETDATE(),
-   Dir_Entrega nvarchar(50) not null,
-   Id_Empleado int,
-   foreign key(Id_Empleado) references Empleado(Id_Empleado),
-   Tipo_Entrega nvarchar(20)
-   )
-   
-   create table Pago(
-    Id_Pago int identity(1,1) primary key not null,
-	Id_Pedido int,
-    foreign key(Id_Pedido) references Pedido(Id_Pedido),
-	ID_Cliente int
-    foreign key (Id_Cliente) references Clientes(Id_Cliente),
-    Metodo_pago nvarchar(20)
-    )
-   
-    
-    create table Topping(
-     Id_Topping int identity(1,1) primary key,
-     Est_Toppping int not null default 1,
-     )
-     
-     create table Extras(
-      Id_Extra int identity(1,1) primary key,
-      Id_Topping int,
-      foreign key(Id_Topping) references Topping(Id_Topping),
-      )
-      
-    
+CREATE TABLE Topping (
+  Id_Topping INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  Ingredientes_Pizza NVARCHAR(60) NOT NULL,
+  Est_Topping INT NOT NULL DEFAULT 1
+)
+
+CREATE TABLE Pizzas (
+  Id_Pizza INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  precio_Pizza MONEY NOT NULL,
+  slice INT NOT NULL,
+  Id_Topping INT,
+  Descripcion NVARCHAR(50),
+  FOREIGN KEY (Id_Topping) REFERENCES Topping(Id_Topping)
+)
+
+CREATE TABLE Extras (
+  Id_Extra INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  Id_Topping INT,
+  FOREIGN KEY (Id_Topping) REFERENCES Topping(Id_Topping)
+)
+
+CREATE TABLE MateriaPrima (
+  IDMateriaPrima INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  Nombre VARCHAR(50),
+  Stock INT,
+  UnidadMedida VARCHAR(20)
+)
+
+CREATE TABLE Pedido (
+  Id_Pedido INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  Num_Pedido INT,
+  Fecha_Pedido DATETIME DEFAULT GETDATE() NOT NULL,
+  Id_Cliente INT,
+  FOREIGN KEY (Id_Cliente) REFERENCES Clientes(Id_Cliente),
+  Id_Pizza INT,
+  FOREIGN KEY (Id_Pizza) REFERENCES Pizzas(Id_Pizza)
+)
+
+CREATE TABLE Pago (
+  Id_Pago INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  Id_Pedido INT,
+  ID_Cliente INT,
+  Metodo_pago NVARCHAR(20),
+  FOREIGN KEY (Id_Pedido) REFERENCES Pedido(Id_Pedido),
+  FOREIGN KEY (ID_Cliente) REFERENCES Clientes(Id_Cliente)
+)
+
+
+CREATE TABLE DetallePedido (
+  Id_DetalleP INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  Id_Pedido INT,
+  Id_Pizza INT,
+  Id_Extras INT,
+  Id_Pago INT,
+  Cantidad INT,
+  PrecioUnitario MONEY,
+  Descuento MONEY,
+  Subtotal MONEY,
+  FOREIGN KEY (Id_Pedido) REFERENCES Pedido(Id_Pedido),
+  FOREIGN KEY (Id_Extras) REFERENCES Extras(Id_Extra),
+  FOREIGN KEY (Id_Pago) REFERENCES Pago(Id_Pago),
+  FOREIGN KEY (Id_Pizza) REFERENCES Pizzas(Id_Pizza)
+)
+
+CREATE TABLE Entrega (
+  ID_Pedido INT,
+  Estado NVARCHAR(20),
+  FechaHoraEntrega DATETIME DEFAULT GETDATE(),
+  Dir_Entrega NVARCHAR(50) NOT NULL,
+  Id_Empleado INT,
+  Id_Cliente INT,
+  Tipo_Entrega NVARCHAR(20),
+  FOREIGN KEY (ID_Pedido) REFERENCES Pedido(Id_Pedido),
+  FOREIGN KEY (Id_Empleado) REFERENCES Empleado(Id_Empleado),
+  FOREIGN KEY (Id_Cliente) REFERENCES Clientes(Id_Cliente)
+)
+
+
+
    
   
     
-   
-   
  
  
  /*que mas podemos agregar?*/
